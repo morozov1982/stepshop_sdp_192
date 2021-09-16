65a906496a17d08ba1239c22f9a6b295342d103f
morozov1982 <morozov1982@mail.com>
2021-09-16 22:26:28 +0600

    update: lesson_08

--- stepshop/mainapp/views.py
+++ stepshop/mainapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
 from mainapp.models import Product, ProductCategory
+from basketapp.models import Basket
 
 
 def products(request, pk=None):
@@ -11,9 +12,15 @@ def products(request, pk=None):
     products_all = Product.objects.all()
     category = {'name': 'продукты'}
 
+    basket = []
+
     if pk is not None:
         products_all = Product.objects.filter(category__id=pk)
         category = get_object_or_404(ProductCategory, id=pk)
+
+
+    if request.user.is_authenticated:
+        basket = Basket.objects.filter(user=request.user)
 
     context = {
         'title': title,
@@ -21,6 +28,7 @@ def products(request, pk=None):
         'products': products_all,
         'category': category,
         'pk': pk,
+        'basket': basket,
     }
 
     return render(request, 'mainapp/products.html', context)
